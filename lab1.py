c11efb4862f4ad11a385b259db059c00c2abf6ee
Griffin <140124176+Snarkwoz@users.noreply.github.com>
2024-05-20 09:50:37 +1000

    almost finished lab 1

--- lab1.py
+++ lab1.py
@@ -12,25 +12,48 @@
 # This class should have an initialiser with at least three attributes. E.g. name, age, and habitat.
 # Add at least two methods for common animal behaviors. E.g. eat and sleep.
 
+class Animal():
+    def __init__ (self,species,age,diet,habitat):
+        self.species = species
+        self.age = age
+        self.diet = diet
+        self.habitat = habitat
 
+    def eat(self):
+        print(f"The {self.species} is eating")
 
-
+    def sleep(self):
+        print(f"The {self.species} is going to sleep. Night night!")
 
 # Create at least two derived classes from the Animal class. E.g. Bird and Fish.
 # Give each of the derived classes at least one specific behavior. E.g. fly and swim.
 
+class Octopus(Animal):
+    def __init__(self,species,age,diet,habitat,arms):
+        super().__init__(species,age,diet,habitat)
+        self.arms = arms
 
+    def swim(self):
+        print(f"The {self.species} is swimming")
 
+    def camoflage(self):
+        (f"The {self.species} has camoflaged into the terrain")
 
-
+class Snake(Animal):
+    def __init__(self,species,age,diet,habitat,length):
+        super().__init__(species,age,diet,habitat)
+        self.length = length
+    
+    def hunt(self):
+        print(f"The {self.species} is hunting")
+    
 # Create at least two instances of the Animal derived classes with different data.
 
-
-
-
+octopus = Octopus("blue ring octopus","6 months","carnivore","reefs","8")
+brown_snake = Snake("brown snake","2 years","carnivore","Australia","1.5m")
 
 # Write code that prints out the details of each animal and calls their specific behaviors.
 
-
+print(f"{octopus.species} is {octopus.age} old, is a {octopus.diet}, lives in {octopus.habitat} and has {octopus.arms} arms")
 
 
